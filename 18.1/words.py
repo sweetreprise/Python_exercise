b266d9bfdd2e15e696402751a56b1712e93ff4f5
maria <86696680+sweetreprise@users.noreply.github.com>
2021-09-03 15:55:23 -0400

    Update and rename words.py to 18.1/words.py

--- 18.1/words.py
+++ 18.1/words.py
@@ -1,5 +1,6 @@
 def print_upper_words(words, must_start_with):
-
+    """ takes in a list of words and capitalizes all of them that
+    start with whatever is passed into must_start_with"""
     for word in words:
         for letter in must_start_with:
             if word.startswith(letter):
